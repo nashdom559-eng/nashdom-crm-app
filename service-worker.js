d86d14cf2bd8e3c731aa5644eb9ec46ba23c06e3
nashdom559-eng <nashdom559@gmail.com>
2026-07-16 17:58:23 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
-const CACHE_NAME = "nashdom-crm-v0.22-photos-objects-1";
+const CACHE_NAME = "nashdom-crm-v0.22.1-photo-reliable-1";
 
 const APP_SHELL = [
   "./",
